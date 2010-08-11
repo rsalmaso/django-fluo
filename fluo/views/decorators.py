85eef0773afba1d0ebc7ad45b13cfc34a31cfd50
Raffaele Salmaso <raffaele@salmaso.org>
2010-08-11 10:40:35 +0200

    correctly indented code

--- fluo/views/decorators.py
+++ fluo/views/decorators.py
@@ -37,9 +37,9 @@ def ajax_required(func):
 
     """
     def wrap(request, *args, **kwargs):
-            if not request.is_ajax():
-                return HttpResponseBadRequest
-            return func(request, *args, **kwargs)
+        if not request.is_ajax():
+            return HttpResponseBadRequest
+        return func(request, *args, **kwargs)
     wrap.__doc__ = func.__doc__
     wrap.__name__ = func.__name__
     return wrap
