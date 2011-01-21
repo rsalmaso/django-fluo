6d97628cc72d62278be15a2b09625fdb04e00677
Raffaele Salmaso <raffaele@salmaso.org>
2011-01-21 11:39:28 +0100

    implemented fluo.forms.SelectYearWidget, to select only a Year

--- fluo/forms/widgets.py
+++ fluo/forms/widgets.py
@@ -20,11 +20,15 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+import datetime
+import re
+from itertools import chain
 from django import forms
-from django.utils.encoding import smart_unicode
-from django.utils.html import escape
+from django.utils.encoding import smart_unicode, force_unicode
+from django.utils.html import escape, conditional_escape
 from django.forms.util import flatatt
 from django.utils.safestring import mark_safe
+from django.utils.dates import MONTHS
 from django.forms.extras.widgets import SelectDateWidget
 from django.contrib.admin.widgets import AdminDateWidget as DateWidget
 from django.contrib.admin.widgets import AdminTimeWidget as TimeWidget
@@ -34,7 +38,10 @@ __all__ = (
     'SelectDateWidget',
     'DateWidget', 'TimeWidget', 'DateTimeWidget',
     'GroupedSelect',
+    'SelectYearWidget',
 )
+
+RE_DATE = re.compile(r'(\d{4})-(\d\d?)-(\d\d?)$')
 
 # taken and adapted from http://djangosnippets.org/snippets/200/
 class GroupedSelect(forms.Select):
@@ -57,4 +64,57 @@ class GroupedSelect(forms.Select):
                 output.append(u'</optgroup>')
         output.append(u'</select>')
         return mark_safe(u'\n'.join(output))
+
+class SelectYearWidget(forms.Widget):
+    none_value = (0, '---')
+    year_field = '%s_year'
+
+    def __init__(self, attrs=None, years=None, required=True):
+        # years is an optional list/tuple of years to use in the "year" select box.
+        self.attrs = attrs or {}
+        self.required = required
+        if years:
+            self.years = years
+        else:
+            this_year = datetime.date.today().year
+            self.years = range(this_year, this_year+10)
+
+    def render(self, name, value, attrs=None):
+        try:
+            year_val = value.year
+        except AttributeError:
+            year_val = month_val = None
+            if isinstance(value, basestring):
+                match = RE_DATE.match(value)
+                if match:
+                    year_val, month_val, day_val = [int(v) for v in match.groups()]
+
+        output = []
+
+        if 'id' in self.attrs:
+            id_ = self.attrs['id']
+        else:
+            id_ = 'id_%s' % name
+
+        year_choices = [(i, i) for i in self.years]
+        if not (self.required and value):
+            year_choices.insert(0, self.none_value)
+        local_attrs = self.build_attrs(id=self.year_field % id_)
+        s = forms.Select(choices=year_choices)
+        select_html = s.render(self.year_field % name, year_val, local_attrs)
+        output.append(select_html)
+
+        return mark_safe(u'\n'.join(output))
+
+    def id_for_label(self, id_):
+        return '%s_year' % id_
+    id_for_label = classmethod(id_for_label)
+
+    def value_from_datadict(self, data, files, name):
+        y = data.get(self.year_field % name)
+        if y == "0":
+            return None
+        if y:
+            return '%s-%s-%s' % (y, 1, 1)
+        return data.get(name, None)
 
