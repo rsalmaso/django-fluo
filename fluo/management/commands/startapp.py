3a7f5fd4eec427e448ed52dbccb752aec1d67a65
Raffaele Salmaso <raffaele@salmaso.org>
2013-08-16 15:48:08 +0200

    override startapp command to use by default fluo app_template files

---
+++ fluo/management/commands/startapp.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2007-2013, Raffaele Salmaso <raffaele@salmaso.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from __future__ import absolute_import, print_function, unicode_literals
+import os
+from django.core.management.commands.startapp import Command as TemplateCommand
+
+class Command(TemplateCommand):
+    """
+    Override default startapp command with fluo app_tempate
+    """
+
+    def handle(self, app_name=None, target=None, **options):
+        if not options['template']:
+            realpath = os.path.realpath(__file__)
+            path = os.path.split(
+                os.path.split(
+                    os.path.split(realpath)[0]
+                )[0]
+            )[0]
+            options['template'] = os.path.join(path, 'conf', 'app_template')
+
+        super(Command, self).handle(app_name, target, **options)
+
