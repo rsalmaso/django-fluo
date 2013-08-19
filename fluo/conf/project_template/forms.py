e1c6c2b78c2cd193bfaa02948d92f522643b20e5
Raffaele Salmaso <raffaele@salmaso.org>
2013-08-19 09:50:33 +0200

    removed project_template, exported in external repository http://bitbucket.org/rsalmaso/django-fluo-project

--- fluo/conf/project_template/forms.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import, print_function, unicode_literals
-#import logging
-#from django.utils.translation import ugettext_lazy as _
-#from fluo import forms
-
-#log = logging.getLogger(__name__)
-
--- fluo/conf/project_template/views.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import, print_function, unicode_literals
-import logging
-from django.utils.translation import ugettext as _
-from django.http import HttpResponse
-
-log = logging.getLogger(__name__)
-
