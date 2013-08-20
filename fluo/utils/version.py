6514402679cb6b17037ed7ec2d2c1b2a8134fcc0
Raffaele Salmaso <raffaele@salmaso.org>
2013-08-20 11:37:54 +0200

    fluo.get_version()

--- fluo/utils/version.py
+++ fluo/utils/version.py
@@ -20,8 +20,29 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-VERSION = (0, 2, 0, 'alpha', 0)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-def get_version(*args, **kwargs):
-    from fluo.utils.version import get_version
-    return get_version(*args, **kwargs)
+def get_version(version=None):
+    "Returns a PEP 386-compliant version number from VERSION."
+    if version is None:
+        from fluo import VERSION as version
+    else:
+        assert len(version) == 5
+        assert version[3] in ('alpha', 'beta', 'rc', 'final')
+
+    # Now build the two parts of the version number:
+    # main = X.Y[.Z]
+    # sub = .devN - for pre-alpha releases
+    #     | {a|b|c}N - for alpha, beta and rc releases
+
+    parts = 2 if version[2] == 0 else 3
+    main = '.'.join(str(x) for x in version[:parts])
+
+    sub = ''
+
+    if version[3] != 'final':
+        mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'c'}
+        sub = mapping[version[3]] + str(version[4])
+
+    return str(main + sub)
+
