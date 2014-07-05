a36ffa3946c92d987a7dbe37702041fab108db5a
Raffaele Salmaso <raffaele@salmaso.org>
2014-07-05 16:18:03 +0200

    admin: removed custom AdminImageFileWidget, use default one

--- fluo/admin/models.py
+++ fluo/admin/models.py
@@ -54,7 +54,6 @@ __all__ = [
 # set this field for all models
 admin.ModelAdmin.save_on_top = True
 admin.options.FORMFIELD_FOR_DBFIELD_DEFAULTS.update({
-    models.ImageField:   {'widget': widgets.AdminImageFileWidget},
     models.OrderField:   {'required': False},
 })
 # end admin customization
