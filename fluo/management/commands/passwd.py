104f784fc51fc704b0923d9eb822987f9dd092cc
Raffaele Salmaso <raffaele@salmaso.org>
2014-07-13 09:39:23 +0200

    commands: update passwd command to query username as ignorecase and an optional password

--- fluo/management/commands/passwd.py
+++ fluo/management/commands/passwd.py
@@ -30,26 +30,25 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Command(BaseCommand):
-    help = "Clone of the UNIX program ``passwd'', for django.contrib.auth."
-
+    help = "Change a user password."
     requires_model_validation = False
 
     def handle(self, *args, **options):
-        if len(args) > 1:
-            raise CommandError("need exactly one or zero arguments for username")
+        if not (0 <= len(args) < 3):
+            raise CommandError("need exactly one or zero arguments for username and eventually a password")
 
-        if args:
-            username, = args
-        else:
-            username = getpass.getuser()
+        username = args[0]
 
         try:
-            u = User.objects.get(username=username)
+            user = User.objects.get(username__iexact=username)
         except User.DoesNotExist:
             raise CommandError("user %s does not exist" % username)
 
-        print("Changing password for user %s" % u.username)
-        p1 = p2 = ""
+        print("Changing password for user %s" % user.username)
+        if len(args) == 2:
+            p1 = p2 = args[1]
+        else:
+            p1 = p2 = ""
         while "" in (p1, p2) or p1 != p2:
             p1 = getpass.getpass()
             p2 = getpass.getpass("Password (again): ")
@@ -58,8 +57,7 @@ class Command(BaseCommand):
             elif "" in (p1, p2):
                 raise CommandError("aborted")
 
-        u.set_password(p1)
-        u.save()
+        user.set_password(p1)
+        user.save()
 
-        return "Password changed successfully for user %s\n" % u.username
-
+        return "Password changed successfully for user %s\n" % user.username
