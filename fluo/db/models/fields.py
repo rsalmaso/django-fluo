dd5241db8b40e228d11d761f90234b204f045131
Raffaele Salmaso <raffaele@salmaso.org>
2015-04-05 07:17:51 +0200

    removed custom UUIDField, now in django >= 1.8

--- fluo/db/models/fields.py
+++ fluo/db/models/fields.py
@@ -25,7 +25,6 @@
 #   - CreationDateTimeField
 #   - ModificationDateTimeField
 #   - AutoSlugField
-#   - UUIDField
 
 # JSONField taken and adapted from https://github.com/bradjasper/django-jsonfield.git
 # Copyright (c) 2012 Brad Jasper
@@ -34,7 +33,6 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import base64
 import copy
 import re
-import uuid
 from django.core import exceptions, validators
 from django.utils import timezone
 from django.utils import six
@@ -50,7 +48,6 @@ __all__ = (
     'CreationDateTimeField', 'ModificationDateTimeField',
     'OrderField',
     'AutoSlugField',
-    'UUIDField',
     'DurationField',
     'JSONField',
     'Base64Field',
@@ -246,61 +243,6 @@ class AutoSlugField(models.SlugField):
         return "SlugField"
 
 
-class UUIDVersionError(Exception):
-    pass
-
-
-class UUIDField(models.CharField):
-    """
-    By default uses UUID version 1 (generate from host ID, sequence number and current time)
-
-    The field support all uuid versions which are natively supported by the uuid python module.
-    For more information see: http://docs.python.org/lib/module-uuid.html
-    """
-
-    def __init__(self, verbose_name=None, name=None, auto=True, version=1, node=None, clock_seq=None, namespace=None, **kwargs): # NOQA
-        kwargs['max_length'] = 36
-        if auto:
-            kwargs['blank'] = True
-            kwargs.setdefault('editable', False)
-        self.auto = auto
-        self.version = version
-        if version == 1:
-            self.node, self.clock_seq = node, clock_seq
-        elif version == 3 or version == 5:
-            self.namespace, self.name = namespace, name
-        super(UUIDField, self).__init__(self, verbose_name, name, **kwargs)
-
-    def get_internal_type(self):
-        return models.CharField.__name__
-
-    def create_uuid(self):
-        if not self.version or self.version == 4:
-            return uuid.uuid4()
-        elif self.version == 1:
-            return uuid.uuid1(self.node, self.clock_seq)
-        elif self.version == 2:
-            raise UUIDVersionError("UUID version 2 is not supported.")
-        elif self.version == 3:
-            return uuid.uuid3(self.namespace, self.name)
-        elif self.version == 5:
-            return uuid.uuid5(self.namespace, self.name)
-        else:
-            raise UUIDVersionError("UUID version %s is not valid." % self.version)
-
-    def pre_save(self, model_instance, add):
-        if self.auto and add:
-            value = unicode(self.create_uuid())
-            setattr(model_instance, self.attname, value)
-            return value
-        else:
-            value = super(UUIDField, self).pre_save(model_instance, add)
-            if self.auto and not value:
-                value = unicode(self.create_uuid())
-                setattr(model_instance, self.attname, value)
-        return value
-
-
 class OrderField(models.IntegerField):
     def __init__(self, *args, **kwargs):
         kwargs['default'] = 0
@@ -472,7 +414,6 @@ def _add_south_support():
             ((CreationDateTimeField,), [], {}),
             ((ModificationDateTimeField,), [], {}),
             ((AutoSlugField,), [], {}),
-            ((UUIDField,), [], {}),
             ((OrderField,), [], {}),
             ((JSONField,), [], {}),
             ((Base64Field,), [], {}),
