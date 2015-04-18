63873990f99d7484aca6bbbfdac629e1c9066311
Raffaele Salmaso <raffaele@salmaso.org>
2015-04-18 20:32:11 +0200

    update passwd command to django 1.8

--- fluo/management/commands/passwd.py
+++ fluo/management/commands/passwd.py
@@ -34,11 +34,12 @@ class Command(BaseCommand):
     help = "Change a user password."
     requires_model_validation = False
 
-    def handle(self, *args, **options):
-        if not (0 <= len(args) < 3):
-            raise CommandError("need exactly one or zero arguments for username and eventually a password")
+    def add_arguments(self, parser):
+        parser.add_argument('username', help='user username')
+        parser.add_argument('password', nargs='?', default='', help='new password')
 
-        username = args[0]
+    def handle(self, *args, **options):
+        username = options.get('username')
 
         try:
             user = User.objects.get(username__iexact=username)
@@ -46,10 +47,10 @@ class Command(BaseCommand):
             raise CommandError("user %s does not exist" % username)
 
         print("Changing password for user %s" % user.username)
-        if len(args) == 2:
-            p1 = p2 = args[1]
-        else:
-            p1 = p2 = ""
+        password = options.get('password')
+
+        p1 = p2 = password
+
         while "" in (p1, p2) or p1 != p2:
             p1 = getpass.getpass()
             p2 = getpass.getpass("Password (again): ")
