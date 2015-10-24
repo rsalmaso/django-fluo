6641f8bc9b6b587377cbb34d425ddfccedd20122
Raffaele Salmaso <raffaele@salmaso.org>
2015-10-24 15:40:56 +0200

    use static to build urls in media_tags

--- fluo/templatetags/media_tags.py
+++ fluo/templatetags/media_tags.py
@@ -21,21 +21,30 @@
 # THE SOFTWARE.
 
 from __future__ import absolute_import, division, print_function, unicode_literals
+from django.apps import apps
 from django import template
 from django.utils.translation import get_language
 from django.utils.encoding import iri_to_uri
-from fluo.settings import STATIC_URL, FLUO_STATIC_URL, JQUERY_STATIC_URL, JQUERY_MINIFIED
+from fluo.settings import MEDIA_URL, JQUERY_MINIFIED
 
+if apps.is_installed('django.contrib.staticfiles'):
+    from django.contrib.staticfiles.templatetags.staticfiles import static as _static
+else:
+    from django.templatetags.static import static as _static
 
 register = template.Library()
 
 
 @register.simple_tag
-def css(script, static="all"):
-    return '<link rel="stylesheet" type="text/css" href="%(static_url)s%(script)s" static="%(static)s"/>' % {
-        'static_url': STATIC_URL,
-        'script': iri_to_uri(script),
-        'static': iri_to_uri(static),
+def static(path):
+    return _static(path)
+
+
+@register.simple_tag
+def css(script, media="all"):
+    return '<link rel="stylesheet" type="text/css" href="%(script)s" media="%(media)s"/>' % {
+        'script': _static(iri_to_uri(script)),
+        'media': media,
     }
 
 
@@ -51,97 +60,96 @@ def css_ie(script, media="all"):
 
 @register.simple_tag
 def js(script):
-    return '<script type="text/javascript" src="%(static)s%(script)s"></script>' % {
-        'static': STATIC_URL,
-        'script': iri_to_uri(script),
+    return '<script type="text/javascript" src="%(script)s"></script>' % {
+        'script': _static(iri_to_uri(script)),
     }
 
 
 @register.simple_tag
 def jquery():
-    return '<script type="text/javascript" src="%(static)s%(jquery)s"></script>' % {
-        'static': JQUERY_STATIC_URL,
+    return js("fluo/jquery/%(jquery)s" % {
         'jquery': {True: 'jquery.min.js', False: 'jquery.js'}[JQUERY_MINIFIED],
-    }
+    })
 
 
 @register.simple_tag
 def jqueryui():
-    return '''<script type="text/javascript" src="%(static)s%(jqueryui)s"></script><script type="text/javascript" src="%(static)si18n/%(i18n)s"></script>''' % { # NOQA
-        'static': JQUERY_STATIC_URL,
-        'jqueryui': {True: 'jquery.ui.min.js', False: 'jquery.ui.js'}[JQUERY_MINIFIED],
-        'i18n': {
-            True: 'jquery.ui.datepicker-%s.min.js',
-            False: 'jquery.ui.datepicker-%s.js',
-        }[JQUERY_MINIFIED] % get_language()[:2],
+    minified = ".min" if JQUERY_MINIFIED else ""
+    return '''
+%(jqueryui)s
+%(i18n)s
+''' % { # NOQA
+        'jqueryui': js("fluo/jquery/jquery.ui{minified}.js".format(minified=minified)),
+        'i18n': js("fluo/jquery/i18n/jquery.ui.datepicker-{language}{minified}.js".format(
+            minified=minified,
+            language=get_language()[:2]
+        )),
     }
 
 
 @register.simple_tag
 def jqueryui_default_theme():
-    return '<link rel="stylesheet" type="text/css" href="%(media_url)stheme/jquery.ui.css" media="all"/>' % {
-        'media_url': JQUERY_STATIC_URL,
-    }
+    return css("fluo/jquery/theme/jquery.ui.css", media="all")
 
 
 @register.simple_tag
 def thickbox():
-    return """<link rel="stylesheet" type="text/css" href="%(static)sthickbox/css/thickbox.css" />
-<script type="text/javascript">var tb_pathToImage = "%(static)sfluo/thickbox/images/loadingAnimation.gif";</script>
-<script type="text/javascript" src="%(static)sfluo/thickbox/js/%(thickbox)s"></script>""" % {
-        'static': FLUO_STATIC_URL,
-        'thickbox': {True: 'thickbox.min.js', False: 'thickbox.js'}[JQUERY_MINIFIED],
+    return """%(css)s
+<script type="text/javascript">var tb_pathToImage = "%(img)s";</script>
+%(js)s""" % {
+        "css": css("fluo/thickbox/css/thickbox.css", media="all"),
+        "img": _static("fluo/thickbox/images/loadingAnimation.gif"),
+        "js": js("fluo/thickbox/js/%(thickbox)s" % {True: 'thickbox.min.js', False: 'thickbox.js'}[JQUERY_MINIFIED]),
     }
 
 
 @register.simple_tag
 def jquery_ajaxqueue():
-    return '<script type="text/javascript" src="%(static)sjquery-ajaxqueue/%(js)s"></script>' % {
-        'static': FLUO_STATIC_URL,
+    return js("fluo/jquery-ajaxqueue/%(js)s" % {
         'js': {True: 'jquery.ajaxqueue.min.js', False: 'jquery.ajaxqueue.js'}[JQUERY_MINIFIED],
-    }
+    })
 
 
 @register.simple_tag
 def jquery_autocomplete():
-    return '<script type="text/javascript" src="%(static)sjquery-autocomplete/%(js)s"></script>' % {
-        'static': FLUO_STATIC_URL,
+    return js("fluo/jquery-autocomplete/%(js)s" % {
         'js': {True: 'jquery.autocomplete.min.js', False: 'jquery.autocomplete.js'}[JQUERY_MINIFIED],
-    }
+    })
 
 
 @register.simple_tag
 def jquery_listreorder():
-    return '<script type="text/javascript" src="%(static)sjquery-listreorder/%(js)s"></script>' % {
-        'static': FLUO_STATIC_URL,
+    return js("fluo/jquery-listreorder/%(js)s" % {
         'js': {True: 'jquery.listreorder.min.js', False: 'jquery.listreorder.js'}[JQUERY_MINIFIED],
-    }
+    })
 
 
 @register.simple_tag
 def jquery_tablednd():
-    return '<script type="text/javascript" src="%(static)sjquery-tablednd/%(js)s"></script>' % {
-        'static': FLUO_STATIC_URL,
+    return js("fluo/jquery-tablednd/%(js)s" % {
         'js': {True: 'jquery.tablednd.min.js', False: 'jquery.tablednd.js'}[JQUERY_MINIFIED],
-    }
+    })
 
 
 @register.simple_tag
 def jquery_bgiframe():
-    return '<script type="text/javascript" src="%(static)sjquery-bgiframe/%(js)s"></script>' % {
-        'static': FLUO_STATIC_URL,
+    return js("fluo/jquery-bgiframe/%(js)s" % {
         'js': {True: 'jquery.bgiframe.min.js', False: 'jquery.bgiframe.js'}[JQUERY_MINIFIED],
-    }
+    })
 
 
 @register.simple_tag
 def jquery_disable_text_select():
-    return '<script type="text/javascript" src="%(static)sjquery-disable-text-select/jquery.disable.text.select.pack.js"></script>' % { # NOQA
-        'static': FLUO_STATIC_URL,
-    }
+    return js("fluo/jquery-disable-text-select/jquery.disable.text.select.pack.js")
+
+
+@register.simple_tag
+def static_url():
+    """ Returns the string contained in the setting STATIC_URL. """
+    return _static("")
 
 
 @register.simple_tag
 def media_url():
-    """ Returns the string contained in the setting STATIC_URL. """
-    return STATIC_URL
+    """ Returns the string contained in the setting MEDIA_URL. """
+    return MEDIA_URL
