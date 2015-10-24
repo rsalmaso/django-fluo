2ef76eba0e73c5d4c0c06ef16c78002c003f6a67
Raffaele Salmaso <raffaele@salmaso.org>
2015-10-24 23:33:04 +0200

    update setup script

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2007-2015, Raffaele Salmaso <raffaele@salmaso.org>
@@ -20,91 +21,43 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-import os
-from os.path import join, dirname
-from setuptools import setup
+from __future__ import absolute_import, print_function
+import io
+from setuptools import setup, find_packages
+import fluo
 
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join)
-    in a platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
 
-# Dynamically calculate the version based on fluo.VERSION.
-version = __import__('fluo').get_version()
+with io.open("README.md", "rU") as f:
+    description = f.read()
 
-EXCLUDE_FROM_PACKAGES = [
-    'fluo.conf.app_template',
-    'fluo.bin',
-]
-def is_package(package_name):
-    for pkg in EXCLUDE_FROM_PACKAGES:
-        if package_name.startswith(pkg):
-            return False
-    return True
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, package_data = [], {}
-
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-fluo_dir = 'fluo'
-
-for dirpath, dirnames, filenames in os.walk(fluo_dir):
-    # Ignore PEP 3147 cache dirs and those whose names start with '.'
-    dirnames[:] = [d for d in dirnames if not d.startswith('.') and d != '__pycache__']
-    parts = fullsplit(dirpath)
-    package_name = '.'.join(parts)
-    if '__init__.py' in filenames and is_package(package_name):
-        packages.append(package_name)
-    elif filenames:
-        relative_path = []
-        while '.'.join(parts) not in packages:
-            relative_path.append(parts.pop())
-        relative_path.reverse()
-        path = os.path.join(*relative_path)
-        package_files = package_data.setdefault('.'.join(parts), [])
-        package_files.extend([os.path.join(path, f) for f in filenames])
 
 setup(
-    name = "django-fluo",
-    description = open(join(dirname(__file__), 'README')).read(),
-    version = version,
-    url = 'https://bitbucket.org/rsalmaso/django-fluo',
-    author = 'Raffaele Salmaso',
-    author_email = 'raffaele@salmaso.org',
-    license='MIT',
-    packages=packages,
-    package_data=package_data,
-    platforms = 'any',
-    classifiers = [
-        'Environment :: Fluo',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Internet :: WWW/HTTP :: WSGI',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Framework :: Django',
-        'Framework :: Fluo',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
+    name="django-fluo",
+    description=description,
+    version=fluo.__version__,
+    url="https://bitbucket.org/rsalmaso/django-fluo",
+    author=fluo.__author__,
+    author_email=fluo.__email__,
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    classifiers=[
+        "Environment :: Fluo",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
+    install_requires=["django"],
+    zip_safe=False,
 )
-
