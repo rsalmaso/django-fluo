d91fee6b6861fae06c40e122cd2a63918c2794b5
Raffaele Salmaso <raffaele@salmaso.org>
2015-12-29 22:13:07 +0100

    admin: reworked ReadOnlyMixin.get_readonly_fields

--- fluo/admin/models.py
+++ fluo/admin/models.py
@@ -270,12 +270,7 @@ class ReadOnlyMixin(object):
     editable_fields = []
 
     def get_readonly_fields(self, request, obj=None):
-        fields = []
-        for field in self.model._meta.get_all_field_names():
-            if (not field == 'id'):
-                if (field not in self.editable_fields):
-                    fields.append(field)
-        return fields
+        return self.fields or [f.name for f in self.model._meta.fields]
 
     def has_add_permission(self, request):
         return False
