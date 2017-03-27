c11baed02a34886e1e8cf34e1387f8ec91f23c18
Raffaele Salmaso <raffaele@salmaso.org>
2017-03-27 23:53:32 +0200

    update version to 0.4.0a4

--- fluo/__init__.py
+++ fluo/__init__.py
@@ -21,7 +21,7 @@
 from .utils.version import get_version
 
 
-VERSION = (0, 4, 0, "alpha", 3)
+VERSION = (0, 4, 0, "alpha", 4)
 
 __version__  = get_version(VERSION)
 __author__  = "Raffaele Salmaso"
