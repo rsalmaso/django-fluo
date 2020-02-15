a3fb4ba5aa7add29df2d222bc6aedf2a73572276
Raffaele Salmaso <raffaele@salmaso.org>
2020-02-15 11:16:30 +0100

    remove startapp command and app_templates

--- fluo/conf/app_template/forms.py
+++
@@ -1,6 +0,0 @@
-# import logging
-# from django.utils.translation import gettext_lazy as _
-# from fluo import forms
-# from .models import X
-
-# log = logging.getLogger('{{ app_name }}')
--- fluo/conf/app_template/tests.py
+++
@@ -1,7 +0,0 @@
-# from django.test import TestCase
-
-# class Test(TestCase):
-# def setUp(self):
-# pass
-# def tearDown(self):
-# pass
